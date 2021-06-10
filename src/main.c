ea9837831ad6098fa647cc22ef986e3600e2aba8
Marek Metelski <marek.metelski@grinn-global.com>
2021-06-10 23:08:21 +0200

    main: Drop 'blinky' code
    Remove example of blinking leds on board from code.

--- src/main.c
+++ src/main.c
@@ -1,60 +1,9 @@
-/*
- * Copyright (c) 2016 Intel Corporation
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include <zephyr.h>
-#include <device.h>
 #include <logging/log.h>
-#include <devicetree.h>
-#include <drivers/gpio.h>
 
 LOG_MODULE_REGISTER(main);
-
-/* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
-
-/* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
-#define LED1_NODE DT_ALIAS(led1)
-
-#if DT_NODE_HAS_STATUS(LED0_NODE, okay)
-#define LED0	DT_GPIO_LABEL(LED0_NODE, gpios)
-#define PIN0	DT_GPIO_PIN(LED0_NODE, gpios)
-#define FLAGS0	DT_GPIO_FLAGS(LED0_NODE, gpios)
-#endif
-
-#if DT_NODE_HAS_STATUS(LED1_NODE, okay)
-#define LED1	DT_GPIO_LABEL(LED1_NODE, gpios)
-#define PIN1	DT_GPIO_PIN(LED1_NODE, gpios)
-#define FLAGS1	DT_GPIO_FLAGS(LED1_NODE, gpios)
-#endif
 
 void main(void)
 {
 	LOG_INF("Hello world!");
-	const struct device *led1;
-	const struct device *led0;
-	bool led_is_on = true;
-	int ret;
-
-	led0 = device_get_binding(LED0);
-	led1 = device_get_binding(LED1);
-	if (led1 == NULL || led0 == NULL) {
-		return;
-	}
-
-	ret = gpio_pin_configure(led0, PIN0, GPIO_OUTPUT_ACTIVE | FLAGS0);
-	ret = gpio_pin_configure(led1, PIN1, GPIO_OUTPUT_ACTIVE | FLAGS1);
-	if (ret < 0) {
-		return;
-	}
-
-	while (1) {
-		gpio_pin_set(led0, PIN0, (int)!led_is_on);
-		gpio_pin_set(led1, PIN1, (int)led_is_on);
-		led_is_on = !led_is_on;
-		k_msleep(1000);
-	}
 }
